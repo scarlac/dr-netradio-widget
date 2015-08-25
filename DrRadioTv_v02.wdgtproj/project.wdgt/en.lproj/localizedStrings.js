8f77ac9c7ae46efcd04a4e08020e49958e0fa6d4
Seph Soliman <seph@seph.dk>
2015-08-25 10:58:00 +0200

    conversion

--- DrRadioTv_v02.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
+++
@@ -1,5 +0,0 @@
-var localizedStrings = new Object;
-
-localizedStrings["Hello, World!"] = "Hello, World!";
-localizedStrings["Done"] = "Done";
-localizedStrings['fullscreen'] = unescape('Fuldsk%u00E6rm');
--- Makefile
+++
@@ -1,9 +0,0 @@
-default: clean widget
-
-widget: clean
-	cp -R src Netradio.wdgt
-	rm -f Netradio.wdgt/css/debug.css
-
-clean:
-	rm -rf Netradio.wdgt
-
